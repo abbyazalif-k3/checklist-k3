6ae0cd47028c165b0d1421ff5b8808e1b1882a75
abbyazalif-k3 <abbyazalif@gmail.com>
2026-08-19 21:31:45 +0800

    Perbarui Service Worker untuk cache terbaru

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
-const CACHE='k3-v5';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'k3-v6';
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE).then(cache =>
+			cache.addAll(['./', './index.html', './manifest.json'])
+		)
+	);
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys().then(keys =>
+			Promise.all(
+				keys
+					.filter(key => key !== CACHE)
+					.map(key => caches.delete(key))
+			)
+		)
+	);
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+	const request = event.request;
+	const isHtmlRequest = request.url.endsWith('/index.html') ||
+		request.mode === 'navigate' ||
+		request.headers.get('accept')?.includes('text/html');
+
+	if (isHtmlRequest) {
+		event.respondWith(
+			fetch(request).then(response => {
+				const copy = response.clone();
+				caches.open(CACHE).then(cache => cache.put(request, copy));
+				return response;
+			}).catch(() => caches.match(request))
+		);
+		return;
+	}
+
+	event.respondWith(
+		caches.match(request).then(response => response || fetch(request))
+	);
+});
